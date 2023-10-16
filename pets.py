78f091aee458d42edda915d44204bc62008f846f
angelikavilde <angelikavilde@gmail.com>
2023-10-16 20:59:29 +0100

    changed how levels change with each hour passed

--- pets.py
+++ pets.py
@@ -16,13 +16,18 @@ class PetAsleep(Exception):
         super().__init__(message)
 
 
+class UnwantedInteraction(Exception):
+    """"""
+    def __init__(self, message="Your pet does not want this interaction right now!"):
+        super().__init__(message)
+
+
 class Pet:
     """"""
 
     def __init__ (self, name, difficulty):
 
         self.name = name
-        self.lifespan_left = 10
         self.difficulty = difficulty
         self.poop_level = 0
         self.hunger_level = 0
@@ -31,26 +36,38 @@ class Pet:
         self.tiredness_level = 0
 
 
-    def check_if_asleep(self):
+    def interact(self):
         """"""
-        if self.asleep["status"]:
-            raise PetAsleep
-
-    def reset_lifespan(self, time_added: int):
-        """"""
-        self.lifespan_left = 10
+        check_if_asleep(self)
+        if self.sadness_level > 0:
+            if self.difficulty == "hard":
+                if self.sadness_level > 0.5:
+                    self.sadness_level -= 0.5
+                else:
+                    self.sadness_level = 0
+            elif self.difficulty == "challenging":
+                if self.sadness_level > 0.75:
+                    self.sadness_level -= 0.75
+                else:
+                    self.sadness_level = 0
+            else:
+                if self.sadness_level > 1:
+                    self.sadness_level -= 1
+                else:
+                    self.sadness_level = 0
+        else:
+            raise UnwantedInteraction
 
     def feed(self):
         """"""
-        self.reset_lifespan()
+        check_if_asleep(self)
+        if self.hunger_level > 0:
+            self.hunger_level -= 1
+        else:
+            raise UnwantedInteraction
 
     def play(self):
-        """"""
-        self.check_if_asleep()
-        if self.difficulty == "hard":
-            if self.sadness_level > 0:
-                self.sadness_level -= 1
-                self.reset_lifespan(0.1)
+        self.interact()
 
     def clean(self):
         """"""
@@ -61,11 +78,7 @@ class Pet:
 
     def pet(self):
         """"""
-        self.check_if_asleep()
-        if self.difficulty == "hard":
-            if self.sadness_level > 0:
-                self.sadness_level -= 1
-                self.reset_lifespan(0.1)
+        self.interact()
 
     def sleep(self):
         """"""
@@ -75,19 +88,6 @@ class Pet:
         else:
             self.asleep["status"] = False
             self.asleep["time"] = None
-
-    def check_if_alive(self) -> bool:
-        """"""
-        if self.difficulty != "easy":
-            self_levels = [self.sadness_level,
-                self.poop_level, self.tiredness_level]
-            if any(level == 10 for level in self_levels):
-                return False
-        if not self.lifespan_left:
-            return False
-        if self.hunger_level == 10:
-            return False
-        return True
 
     def poop(self):
         """"""
@@ -104,6 +104,17 @@ class Pet:
         if level_change(easy=15, challenging=40, hard=80, difficulty=self.difficulty):
             self.sadness_level += 1
 
+    def tiredness(self):
+        """"""
+        if level_change(easy=10, challenging=30, hard=60, difficulty=self.difficulty):
+            self.tiredness_level += 1
+
+
+def check_if_asleep(pet: Pet) -> None:
+    """"""
+    if pet.asleep["status"]:
+        raise PetAsleep
+
 
 def level_change(easy: int, challenging: int, hard: int, difficulty: str) -> bool:
     """"""
@@ -115,5 +126,26 @@ def level_change(easy: int, challenging: int, hard: int, difficulty: str) -> boo
     return False
 
 
+def check_if_alive(pet: Pet) -> bool:
+    """"""
+    self_levels = [pet.sadness_level,
+            pet.poop_level, pet.tiredness_level]
+    if pet.difficulty == "hard":
+        if any(level == 10 for level in self_levels):
+            return False
+    if pet.difficulty == "challenging":
+        if any(level == 20 for level in self_levels):
+            return False
+    if pet.hunger_level == 10:
+        return False
+    return True
+
+
 if __name__ == "__main__":
     dog = Pet("test", "hard")
+    # every hour:
+    while (check_if_alive(dog)):
+        dog.poop()
+        dog.hunger()
+        dog.boredom()
+        dog.tiredness()
